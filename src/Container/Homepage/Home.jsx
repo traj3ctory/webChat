fbcc9b5be16541c6435bbbe2a4141a9e488d06d7
traj3ctory <davidbenard.bd@gmail.com>
2020-10-15 16:12:59 +0100

    minor update

--- src/Container/Homepage/Home.jsx
+++ src/Container/Homepage/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Layout from '../../Component/Layout/Layout';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+import "./Home.css";
 
 /**
 * @author
@@ -10,7 +12,31 @@ const Home = (props) => {
 
   return (
     <Layout>
-      <div>Home</div>
+      <Container className="mt-5">
+        <Card className="shadow-sm">
+        <Row>
+          <Col md="3">
+            <Card.Body className="list">
+              <ListGroup variant="flush">
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+                <ListGroup.Item>Hello World</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Col>
+          <Col md="9">
+            <Card.Body className="mssgs">
+
+            </Card.Body>
+          </Col>
+        </Row>
+        </Card>
+      </Container>
     </Layout>
   )
 
